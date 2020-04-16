c95a4340553a77bcd721659f2d68909015bfad01
Joe McAbee <jmcabeejr@gmail.com>
2020-04-16 10:48:17 -0400

    Create new function to calculate total value.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "1~2~~3";
+            var output = CalculateTotal(input);
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine($"Output: {output}");
+
+            input = "~~~";
+            output = CalculateTotal(input);
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine($"Output: {output}");
+
+            input = "~1~00~12~~~";
+            output = CalculateTotal(input);
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine($"Output: {output}");
+        }
+
+        private static int CalculateTotal(string input)
+        {
+            var output = 0;
+
+            //1~2~3
+            var inputArrary = input.Split('~', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in inputArrary)
+            {
+                int.TryParse(item, out int value);
+                output += value;
+            }
+
+            return output;
+        }
+    }
+}
